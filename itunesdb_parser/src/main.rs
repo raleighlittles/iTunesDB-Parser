85b3b7bd8605647193608a9c5faaabb899369df3
Raleigh Littles III <raleighlittles@gmail.com>
2023-04-28 12:56:59 -0700

    now detects number of images

--- itunesdb_parser/src/main.rs
+++ itunesdb_parser/src/main.rs
@@ -13,6 +13,10 @@ fn main() {
 
     // TODO: Move these to another module
     let image_list_key = "mhli";
+    let image_list_key_ascii = image_list_key.as_bytes();
+
+    let image_list_num_images_offset = 8; // 4 + 4
+    let image_list_num_images_length = 4;
 
     if itunesdb_file_type == "photo" {
 
@@ -35,8 +39,16 @@ fn main() {
             //     println!("Image list found!");
             // }
 
-            if (db_file_as_bytes[idx] == 0x6D) && (db_file_as_bytes[idx + 1] == 0x68) && (db_file_as_bytes[idx + 2] == 0x6C) && (db_file_as_bytes[idx + 3] == 0x69) {
-                println!("Image list found, starting at idx {}", idx)
+            if (db_file_as_bytes[idx] == image_list_key_ascii[0]) && 
+            (db_file_as_bytes[idx + 1] == image_list_key_ascii[1]) && 
+            (db_file_as_bytes[idx + 2] == image_list_key_ascii[2]) && 
+            (db_file_as_bytes[idx + 3] == image_list_key_ascii[3]) {
+
+                //println!("Image list found, starting at idx {}", idx);
+
+                let image_list_num_images : u32 = db_file_as_bytes[idx + image_list_num_images_offset .. idx + image_list_num_images_offset + image_list_num_images_length].iter().map(|i| (*i) as u32).sum();
+
+                println!("{} images found", image_list_num_images);
             }
 
             idx = idx + SUBSTRUCTURE_SIZE;
