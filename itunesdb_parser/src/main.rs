68674322b20f8c3246ae7bfa86edc793388c5998
Raleigh Littles III <raleighlittles@gmail.com>
2023-04-29 00:19:18 -0700

    Working on 'Photo Album' keys

--- itunesdb_parser/src/main.rs
+++ itunesdb_parser/src/main.rs
@@ -12,11 +12,63 @@ fn main() {
     const SUBSTRUCTURE_SIZE : usize = 4;
 
     // TODO: Move these to another module
-    let image_list_key = "mhli";
-    let image_list_key_ascii = image_list_key.as_bytes();
+    // ==================================
+    // ----- IMAGE ITEM ----- 
+    const image_list_key : &str = "mhli";
+    const image_list_key_ascii : &[u8] = image_list_key.as_bytes();
 
-    let image_list_num_images_offset = 8; // 4 + 4
-    let image_list_num_images_length = 4;
+    const image_list_num_images_offset : u8 = 8; // 4 + 4
+    const image_list_num_images_len : u8 = 4;
+
+    const image_item_key : &str = "mhii";
+    const image_item_key_ascii : &[u8] = image_item_key.as_bytes();
+
+    const image_item_rating_offset : u8 = 32; // 4 * 8
+    const image_item_rating_len : u8 = 4;
+
+    const image_item_orig_date_offset : u8 = image_item_rating_offset + image_item_rating_len + 4;
+    const image_item_orig_date_len : u8 = 4;
+
+    const image_item_digitized_date_offset : u8 = image_item_orig_date_offset + image_item_orig_date_len;
+    const image_item_digitized_date_len : u8 = 4;
+
+    const image_item_source_img_size_offset : u8 = image_item_digitized_date_offset + image_item_digitized_date_len;
+    const image_item_source_img_size_len : u8 = 4;
+
+    // ----- IMAGE NAME -----
+    const image_name_key : &str = "mhni";
+    const image_name_key_ascii : &[u8] = image_name_key.as_bytes();
+
+    // TODO ~ There's 2 size fields in this key list, and I don't understand what the difference between the two is.
+    // There's also another image size field in the "Image Item" key list. I don't know the difference between this
+    // and that one either.
+    // There's also a table that indicates you can determine the format of the image itself (eg UYVY, RGB, etc) from the size,
+    // but I don't know which size field it's referring to.
+    const image_name_img_size_offset : u8 = 24; // 4 * 6
+    const image_name_img_size_len : u8 = 4;
+
+    const image_name_img_height_offset : u8 = 32; // 4 * 8
+    const image_name_img_height_len : u8 = 2;
+
+    const image_name_img_width_offset : u8 = image_name_img_height_offset + image_name_img_height_len;
+    const image_name_img_width_len : u8 = image_name_img_height_len;
+
+
+    // ----- PHOTO ALBUM -----
+    const photo_album_key : &str = "mhba";
+    const photo_album_key_ascii : &[u8] = photo_album_key.as_bytes();
+
+    const photo_album_album_item_cnt_offset 
+
+
+
+
+
+
+
+    // ----------------------- End key name definitions
+
+
 
     if itunesdb_file_type == "photo" {
 
@@ -50,6 +102,8 @@ fn main() {
 
                 println!("{} images found", image_list_num_images);
             }
+
+
 
             idx = idx + SUBSTRUCTURE_SIZE;
         }
