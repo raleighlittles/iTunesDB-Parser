73551082f243d3e3a8dbdf54f87eb25cfe7a8177
Raleigh Littles III <raleighlittles@gmail.com>
2023-06-29 23:07:30 -0700

    adding all the new constants for the music file
    Currently on , start at offset after 88

--- itunesdb_parser/src/itunesdb.rs
+++ itunesdb_parser/src/itunesdb.rs
@@ -1,14 +1,16 @@
 /**
- * File: constants.rs
+ * File: itunesdb.rs
  *
- * Provides the iTunesDB constants. See itunesdb-doc for more info.
+ * Provides the iTunesDB constants, and associated functionality. See itunesdb-doc for more info.
  * Constants are split into "sections", ie 'Image List', 'Image Item'. Each section has a header,
  * and within that header are fields, which have two values associated with them:
  * (1) an offset -- where it is found in the header structure
  * (2) a length.
  *
- * Each section has a "last offset" value that indicates the offset of the last item in the header.
+ * Each section of constants has a "last offset" value that indicates the offset of the last item in the header.
  */
+
+use chrono::{DateTime, NaiveDateTime, Utc};
 
 pub mod itunesdb_constants {
 
@@ -16,7 +18,6 @@ pub mod itunesdb_constants {
 
     // ----- IMAGE LIST ----- //
     pub const IMAGE_LIST_KEY: &str = "mhli";
-    pub const IMAGE_LIST_KEY_ASCII: &[u8] = IMAGE_LIST_KEY.as_bytes();
 
     pub const IMAGE_LIST_NUM_IMAGES_OFFSET: usize = 8; // 4 + 4
     pub const IMAGE_LIST_NUM_IMAGES_LEN: usize = 4;
@@ -25,7 +26,6 @@ pub mod itunesdb_constants {
 
     // ----- IMAGE ITEM ----- //
     pub const IMAGE_ITEM_KEY: &str = "mhii";
-    pub const IMAGE_ITEM_KEY_ASCII: &[u8] = IMAGE_ITEM_KEY.as_bytes();
 
     pub const IMAGE_ITEM_RATING_OFFSET: usize = 32; // 4 * 8
     pub const IMAGE_ITEM_RATING_LEN: usize = 4;
@@ -46,7 +46,6 @@ pub mod itunesdb_constants {
 
     // ----- IMAGE NAME ----- //
     pub const IMAGE_NAME_KEY: &str = "mhni";
-    pub const IMAGE_NAME_KEY_ASCII: &[u8] = IMAGE_NAME_KEY.as_bytes();
 
     // TODO #1 ~ There's 2 size fields in this key list, and I don't understand what the difference between the two is.
     // There's also another image size field in the "Image Item" key list. I don't know the difference between this
@@ -67,7 +66,6 @@ pub mod itunesdb_constants {
 
     // ----- PHOTO ALBUM ----- //
     pub const PHOTO_ALBUM_KEY: &str = "mhba";
-    pub const PHOTO_ALBUM_KEY_ASCII: &[u8] = PHOTO_ALBUM_KEY.as_bytes();
 
     pub const PHOTO_ALBUM_ALBUM_ITEM_CNT_OFFSET: usize = 16; // 4 * 4
     pub const PHOTO_ALBUM_ALBUM_ITEM_CNT_LEN: usize = 4;
@@ -76,7 +74,6 @@ pub mod itunesdb_constants {
 
     // ----- DATA OBJECT ----- //
     pub const DATA_OBJECT_KEY: &str = "mhod";
-    pub const DATA_OBJECT_KEY_ASCII: &[u8] = DATA_OBJECT_KEY.as_bytes();
 
     pub const DATA_OBJECT_HEADER_LENGTH: usize = 0x18;
 
@@ -96,8 +93,95 @@ pub mod itunesdb_constants {
         DATA_OBJECT_STRING_SUBCONTAINER_ENCODING_OFFSET + 4;
 
     pub const DATA_OBJECT_LAST_OFFSET: usize = 16; // 4 * 4
+
+    // ----- DATABASE OBJECT ----- //
+    pub const DATABASE_OBJECT_KEY : &str = "mhbd";
+
+    // 4×8+2+8+2+20
+    pub const DATABASE_OBJECT_LANGUAGE_OFFSET : usize = 64;
+    pub const DATABASE_OBJECT_LANGUAGE_LEN : usize = 2;
+
+    // ----- TRACK LIST ----- //
+    pub const TRACK_LIST_KEY : &str = "mhlt";
+
+    pub const TRACK_LIST_NUM_SONGS_OFFSET : usize = 8;
+    pub const TRACK_LIST_NUM_SONGS_LEN : usize = 4;
+
+    pub const TRACK_LIST_LAST_OFFSET : usize = 12;
+
+    // ----- TRACK_ITEM ----- //
+    pub const TRACK_ITEM_KEY : &str = "mhit";
+
+    pub const TRACK_ITEM_TRACK_FILETYPE_OFFSET : usize = 24;
+    pub const TRACK_ITEM_TRACK_FILETYPE_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_BITRATE_SETTING_OFFSET : usize = 28;
+    pub const TRACK_ITEM_TRACK_BITRATE_SETTING_LEN : usize = 1;
+
+    pub const TRACK_ITEM_IS_COMPILATION_OFFSET : usize = 30;
+    pub const TRACK_ITEM_IS_COMPILATION_LEN : usize = 1;
+
+    pub const TRACK_ITEM_TRACK_RATING_OFFSET : usize = 31;
+    pub const TRACK_ITEM_TRACK_RATING_LEN : usize = 1;
+
+    pub const TRACK_ITEM_TRACK_MODIFIED_TIME_OFFSET : usize = 32;
+    pub const TRACK_ITEM_TRACK_MODIFIED_TIME_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_FILE_SIZE_BYTES_OFFSET : usize = 36;
+    pub const TRACK_ITEM_TRACK_FILE_SIZE_BYTES_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_LENGTH_MILLISECONDS_OFFSET : usize = 40;
+    pub const TRACK_ITEM_TRACK_LENGTH_MILLISECONDS_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_NUMBER_OFFSET : usize = 44;
+    pub const TRACK_ITEM_TRACK_NUMBER_LEN  : usize = 4;
+
+    pub const TRACK_ITEM_NUM_TRACKS_IN_ALBUM_OFFSET : usize = 48;
+    pub const TRACK_ITEM_NUM_TRACKS_IN_ALBUM_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_YEAR_PUBLISHED_OFFSET : usize = 52;
+    pub const TRACK_ITEM_TRACK_YEAR_PUBLISHED_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_BITRATE_OFFSET : usize = 56;
+    pub const TRACK_ITEM_TRACK_BITRATE_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_SAMPLE_RATE_OFFSET : usize = 60;
+    pub const TRACK_ITEM_TRACK_SAMPLE_RATE_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_VOLUME_OFFSET : usize = 64;
+    pub const TRACK_ITEM_TRACK_VOLUME_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_START_TIME_OFFSET : usize = 68;
+    pub const TRACK_ITEM_TRACK_START_TIME_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_STOP_TIME_OFFSET : usize = 72;
+    pub const TRACK_ITEM_TRACK_STOP_TIME_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_PLAY_COUNT_OFFSET : usize = 80;
+    pub const TRACK_ITEM_TRACK_PLAY_COUNT_LEN : usize = 4;
+
+    pub const TRACK_ITEM_TRACK_LAST_PLAYED_TIMESTAMP_OFFSET : usize = 88;
+    pub const TRACK_ITEM_TRACK_LAST_PLAYED_LEN : usize = 4;
+
+
+
+
+
+    pub const TRACK_ITEM_LAST_OFFSET : usize = 356;
+
+
 }
 
-/// Mac timestamps start on Jan 1 1904, whereas Linux timestamps (which is what Rust's library uses)
-/// start at Jan 1 1970, hence this difference
-pub const MAC_TO_LINUX_EPOCH_CONVERSION: i64 = 2082844800;
+
+pub mod itunesdb_helpers {
+
+    pub fn get_timestamp_as_mac(mac_timestamp : u64) -> chrono::DateTime<chrono::Utc> {
+
+        return chrono::DateTime::<chrono::Utc>::from_utc( chrono::NaiveDateTime::from_timestamp_opt((mac_timestamp as i64) - super::MAC_TO_LINUX_EPOCH_CONVERSION, 0).unwrap(), chrono::offset::Utc);
+    }
+
+}
+/// Mac timestamps start on Jan 1 1904, whereas Linux timestamps
+/// (which is what Rust's `chrono` library uses) start at Jan 1 1970,
+/// hence this difference
+ const MAC_TO_LINUX_EPOCH_CONVERSION: i64 = 2082844800;
--- itunesdb_parser/src/main.rs
+++ itunesdb_parser/src/main.rs
@@ -1,8 +1,9 @@
-mod constants;
+mod itunesdb;
 mod helpers;
 
-use crate::constants::itunesdb_constants::*;
-use chrono::{DateTime, NaiveDateTime, Utc};
+use crate::itunesdb::itunesdb_constants::*;
+use crate::itunesdb::itunesdb_helpers;
+use crate::itunesdb::itunesdb_helpers::*;
 
 fn main() {
     let itunesdb_filename: String = std::env::args()
@@ -21,15 +22,18 @@ fn main() {
     let mut num_data_objects = 0;
 
     if itunesdb_file_type == "photo" {
+
         let mut idx = 0;
 
         while idx < db_file_as_bytes.len() {
-            // Parse Image List
 
-            if (db_file_as_bytes[idx] == IMAGE_LIST_KEY_ASCII[0])
-                && (db_file_as_bytes[idx + 1] == IMAGE_LIST_KEY_ASCII[1])
-                && (db_file_as_bytes[idx + 2] == IMAGE_LIST_KEY_ASCII[2])
-                && (db_file_as_bytes[idx + 3] == IMAGE_LIST_KEY_ASCII[3])
+            let potential_section_heading = &db_file_as_bytes[idx .. idx + 3];
+
+            // if (db_file_as_bytes[idx] == IMAGE_LIST_KEY_ASCII[0])
+            //     && (db_file_as_bytes[idx + 1] == IMAGE_LIST_KEY_ASCII[1])
+            //     && (db_file_as_bytes[idx + 2] == IMAGE_LIST_KEY_ASCII[2])
+            //     && (db_file_as_bytes[idx + 3] == IMAGE_LIST_KEY_ASCII[3])
+            if potential_section_heading == IMAGE_LIST_KEY.as_bytes()
             {
                 let image_list_num_images_raw = &db_file_as_bytes[idx + IMAGE_LIST_NUM_IMAGES_OFFSET
                     ..idx + IMAGE_LIST_NUM_IMAGES_OFFSET + IMAGE_LIST_NUM_IMAGES_LEN];
@@ -38,7 +42,7 @@ fn main() {
                     helpers::helpers::build_le_u32_from_bytes(image_list_num_images_raw);
 
                 println!(
-                    "ImageList#{} info... NumImages={}",
+                    "ImageList#{} info : NumImages={}",
                     num_image_lists, image_list_num_images
                 );
                 println!("==========");
@@ -48,10 +52,7 @@ fn main() {
                 idx = idx + IMAGE_LIST_LAST_OFFSET;
             }
             // Parse Image Item
-            else if (db_file_as_bytes[idx] == IMAGE_ITEM_KEY_ASCII[0])
-                && (db_file_as_bytes[idx + 1] == IMAGE_ITEM_KEY_ASCII[1])
-                && (db_file_as_bytes[idx + 2] == IMAGE_ITEM_KEY_ASCII[2])
-                && (db_file_as_bytes[idx + 3] == IMAGE_ITEM_KEY_ASCII[3])
+            else if potential_section_heading == IMAGE_ITEM_KEY.as_bytes()
             {
                 let image_item_rating_raw: &[u8] = &db_file_as_bytes[idx + IMAGE_ITEM_RATING_OFFSET
                     ..idx + IMAGE_ITEM_RATING_OFFSET + IMAGE_ITEM_RATING_LEN];
@@ -65,15 +66,7 @@ fn main() {
                 let image_item_orig_date_timestamp =
                     helpers::helpers::build_le_u32_from_bytes(image_item_orig_date_raw);
 
-                let image_item_orig_date_date = DateTime::<Utc>::from_utc(
-                    NaiveDateTime::from_timestamp_opt(
-                        image_item_orig_date_timestamp as i64
-                            - constants::MAC_TO_LINUX_EPOCH_CONVERSION,
-                        0,
-                    )
-                    .unwrap(),
-                    Utc,
-                );
+                let image_item_orig_date_date = itunesdb_helpers::get_timestamp_as_mac(image_item_orig_date_timestamp as u64);
 
                 let image_item_digitized_date_raw: &[u8] = &db_file_as_bytes[idx
                     + IMAGE_ITEM_DIGITIZED_DATE_OFFSET
@@ -81,15 +74,8 @@ fn main() {
 
                 let image_item_digitized_date_timestamp: u32 =
                     helpers::helpers::build_le_u32_from_bytes(image_item_digitized_date_raw);
-                let image_item_digitized_date_date = DateTime::<Utc>::from_utc(
-                    NaiveDateTime::from_timestamp_opt(
-                        image_item_digitized_date_timestamp as i64
-                            - constants::MAC_TO_LINUX_EPOCH_CONVERSION,
-                        0,
-                    )
-                    .unwrap(),
-                    Utc,
-                );
+
+                let image_item_digitized_date_date = itunesdb_helpers::get_timestamp_as_mac(image_item_digitized_date_timestamp as u64);
 
                 let image_item_source_img_size_raw = &db_file_as_bytes[idx
                     + IMAGE_ITEM_SOURCE_IMG_SIZE_OFFSET
@@ -98,7 +84,7 @@ fn main() {
                 let image_item_source_img_size =
                     helpers::helpers::build_le_u32_from_bytes(image_item_source_img_size_raw);
 
-                println!("ImageItem#{} info... Rating= {} , ImgSize= {}, OrigDateTS= {} , DigitizedDateTS= {}", num_image_items, image_item_rating, image_item_source_img_size, image_item_orig_date_date, image_item_digitized_date_date);
+                println!("ImageItem#{} info : Rating= {} , ImgSize= {}, OrigDateTS= {} , DigitizedDateTS= {}", num_image_items, image_item_rating, image_item_source_img_size, image_item_orig_date_date, image_item_digitized_date_date);
 
                 println!("==========");
                 num_image_items += 1;
@@ -106,10 +92,7 @@ fn main() {
                 idx = idx + IMAGE_ITEM_LAST_OFFSET;
             }
             // Parse Image Name
-            else if (db_file_as_bytes[idx] == IMAGE_NAME_KEY_ASCII[0])
-                && (db_file_as_bytes[idx + 1] == IMAGE_NAME_KEY_ASCII[1])
-                && (db_file_as_bytes[idx + 2] == IMAGE_NAME_KEY_ASCII[2])
-                && (db_file_as_bytes[idx + 3] == IMAGE_NAME_KEY_ASCII[3])
+            else if potential_section_heading == IMAGE_NAME_KEY.as_bytes()
             {
                 let image_name_img_size_raw = &db_file_as_bytes[idx + IMAGE_NAME_IMG_SIZE_OFFSET
                     ..idx + IMAGE_NAME_IMG_SIZE_OFFSET + IMAGE_NAME_IMG_SIZE_LEN];
@@ -129,7 +112,7 @@ fn main() {
                     helpers::helpers::build_le_u32_from_bytes(image_name_img_width_raw);
 
                 println!(
-                    "ImageName#{} info... Size(bytes)={} , Height={} , Width={}",
+                    "ImageName#{} info : Size(bytes)={} , Height={} , Width={}",
                     num_image_names,
                     image_name_img_size,
                     image_name_img_height,
@@ -142,10 +125,7 @@ fn main() {
                 idx = idx + IMAGE_NAME_LAST_OFFSET;
             }
             // Parse Photo Album
-            else if (db_file_as_bytes[idx] == PHOTO_ALBUM_KEY_ASCII[0])
-                && (db_file_as_bytes[idx + 1] == PHOTO_ALBUM_KEY_ASCII[1])
-                && (db_file_as_bytes[idx + 2] == PHOTO_ALBUM_KEY_ASCII[2])
-                && (db_file_as_bytes[idx + 3] == PHOTO_ALBUM_KEY_ASCII[3])
+            else if potential_section_heading == PHOTO_ALBUM_KEY.as_bytes()
             {
                 let photo_album_item_cnt_raw = &db_file_as_bytes[idx
                     + PHOTO_ALBUM_ALBUM_ITEM_CNT_OFFSET
@@ -155,7 +135,7 @@ fn main() {
                     helpers::helpers::build_le_u32_from_bytes(photo_album_item_cnt_raw);
 
                 println!(
-                    "PhotoAlbum#{} info... Item count#={}",
+                    "PhotoAlbum#{} info : Item count#={}",
                     num_photo_albums, photo_album_item_cnt
                 );
 
@@ -165,10 +145,7 @@ fn main() {
                 idx = idx + PHOTO_ALBUM_LAST_OFFSET;
             }
             // Parse Data Object
-            else if (db_file_as_bytes[idx] == DATA_OBJECT_KEY_ASCII[0])
-                && (db_file_as_bytes[idx + 1] == DATA_OBJECT_KEY_ASCII[1])
-                && (db_file_as_bytes[idx + 2] == DATA_OBJECT_KEY_ASCII[2])
-                && (db_file_as_bytes[idx + 3] == DATA_OBJECT_KEY_ASCII[3])
+            else if potential_section_heading == DATA_OBJECT_KEY.as_bytes()
             {
                 let data_object_type_raw = &db_file_as_bytes[idx + DATA_OBJECT_TYPE_OFFSET
                     ..idx + DATA_OBJECT_TYPE_OFFSET + DATA_OBJECT_TYPE_LEN];
@@ -211,6 +188,7 @@ fn main() {
                         println!("MHOD substring (UTF-8) = {}", data_object_subcontainer_data);
                     } else if data_object_subcontainer_encoding == 2 {
                         // Build UTF-16 array, out of UTF-8, by combining elements pairwise
+                        // Maybe put this into its own function in helpers
                         let mut data_object_pairwise_combined: Vec<u16> = vec![];
 
                         for i in (idx + DATA_OBJECT_STRING_SUBCONTAINER_DATA_OFFSET + 4
@@ -229,17 +207,17 @@ fn main() {
                             String::from_utf16(&data_object_pairwise_combined)
                                 .expect("Can't convert UTF-16 array to string");
 
-                        println!("MHOD substring (UTF-16)= {}", data_object_subcontainer_data);
+                        println!("MHOD substring (UTF-16) = {}", data_object_subcontainer_data);
                     }
 
                     println!(
-                        "String MHOD detected... Len={}, Encoding (raw)={}",
+                        "String MHOD detected : Length={}, Encoding (raw)={}",
                         data_object_subcontainer_str_len, data_object_subcontainer_encoding
                     );
                 }
 
                 println!(
-                    "DataObject#{} info... Type={}",
+                    "DataObject#{} info : Type={}",
                     num_data_objects, data_object_type
                 );
 
@@ -251,7 +229,13 @@ fn main() {
 
             idx = idx + SUBSTRUCTURE_SIZE;
         }
-    } else {
-        println!("ERROR: Only the 'photo' file parser is currently implemented.");
+    } else if itunesdb_file_type == "music" {
+
+        let mut idx = 0;
+
+        while idx < db_file_as_bytes.len() {
+
+        }
+        
     }
 }
