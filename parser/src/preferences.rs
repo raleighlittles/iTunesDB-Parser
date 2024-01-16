de34d45aae40686e8f98781876d1cc869a98c443
Raleigh Littles III <raleighlittles@gmail.com>
2024-01-15 18:39:01 -0800

    working on defining rest of languages in index

--- parser/src/preferences.rs
+++ parser/src/preferences.rs
@@ -22,3 +22,25 @@ pub fn decode_timezone(raw_timezone_info : u8) -> u8 {
 
     return timezone_hour.0 / 2;
 }
+
+/// This was a little bit tricky to figure out. The index comes from when the device starts up for the first time, and
+/// asks you to choose the language list.
+/// I couldn't find anywhere this was documented so I ended up having to find an old iPod myself, reset it, and scroll
+/// through the list of available languages.
+pub fn decode_language_from_idx(lang_idx : u8) -> String {
+
+    let selected_lang : Option<isolang::Language> = match lang_idx {
+        0_u8 | 1_u8 => isolang::Language::from_639_1("en"),
+        2_u8 => isolang::Language::from_639_1("ja"),
+        3_u8 => isolang::Language::from_639_1("cs"),
+        4_u8 => isolang::Language::from_639_1("da"),
+        5_u8 => isolang::Language::from_639_1("de"),
+        6_u8 => isolang::Language::from_639_1("es"),
+        7_u8 => isolang::Language::from_639_1("fr"),
+        8_u8 => isolang::Language::from_639_1("el"),
+        9_u8 => isolang::Language::from_639_1("hr"),
+        
+    };
+
+    return selected_lang.unwrap().to_name().to_string();
+}
