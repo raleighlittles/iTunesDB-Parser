1c676dd97961fb96c43f2e7138603f95006a2b7b
Raleigh Littles <raleighlittles@gmail.com>
2024-11-26 12:03:25 -0700

    encountering API issues with playlist not sure why

--- spotify_integration/spotify_integration.py
+++ spotify_integration/spotify_integration.py
@@ -3,12 +3,14 @@ import spotipy
 import argparse
 import socket
 import json
+import datetime
+
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
 UNITED_STATES_ISO_3166_1_CODE = "US"
 DEFAULT_QUERY_LIMIT = 3
-DEFAULT_SPOTIFY_API_SCOPE="playlist-modify-private"
+DEFAULT_SPOTIFY_API_SCOPE = "playlist-modify-private"
 
 def get_track_ids_from_csv(csv_file, spotify_api_obj) -> list:
 
@@ -18,7 +20,8 @@ def get_track_ids_from_csv(csv_file, spotify_api_obj) -> list:
     with open(csv_file, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
 
-        sp_obj = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=spotify_api_obj["client_id"],client_secret=spotify_api_obj["client_secret"]))
+        sp_obj = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+            client_id=spotify_api_obj["client_id"], client_secret=spotify_api_obj["client_secret"]))
 
         for csv_row in csv_reader:
             song_title = csv_row[argparse_args.track_column]
@@ -41,19 +44,18 @@ def get_track_ids_from_csv(csv_file, spotify_api_obj) -> list:
                 track_id = search_results["tracks"]["items"][0]["id"]
                 spotify_tracks.append(
                     dict(song_title=song_title, song_artist=song_artist, track_id=track_id))
-                
+
     print(f"[DEBUG] Found {num_songs_found} songs on Spotify")
     return spotify_tracks
 
 
 def create_playlist_from_tracks(spotify_tracks: list, playlist_name: str, playlist_description: str, spotify_api_obj) -> str:
 
-    sp_obj = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_api_obj["client_id"],
-                                                       client_secret=spotify_api_obj["client_secret"], redirect_uri=spotify_api_obj["redirect_uri"], scope=DEFAULT_SPOTIFY_API_SCOPE))
+    sp_obj = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_api_obj["client_id"], client_secret=spotify_api_obj["client_secret"], redirect_uri=spotify_api_obj["redirect_uri"], scope=DEFAULT_SPOTIFY_API_SCOPE))
 
     user_id = sp_obj.current_user()["id"]
     playlist_id = sp_obj.user_playlist_create(
-        user_id, playlist_name, public=False, description=socket.gethostname())["id"]
+        user_id, playlist_name, public=False, description=playlist_description)["id"]
 
     track_ids = [track["track_id"] for track in spotify_tracks]
     sp_obj.playlist_add_items(playlist_id, track_ids)
@@ -69,23 +71,27 @@ if __name__ == '__main__':
         "-f", "--csv-file", help="Path to the CSV file containing the songs", required=True)
     argparse_parser.add_argument(
         "-t", "--track-column", help="Name of the column containing the track names", required=False, default="Song Title")
-    argparse_parser.add_argument("-a", "--api-credentials-file", help="Path to JSON file containing API credentials, see spotify_api_credentials.json for the format", required=True)
+    argparse_parser.add_argument("-a", "--api-credentials-file",
+                                 help="Path to JSON file containing API credentials, see spotify_api_credentials.json for the format", required=True)
     argparse_args = argparse_parser.parse_args()
 
     if not argparse_args.csv_file:
         raise FileNotFoundError(
             f"Error: CSV file '{argparse_args.csv_file}' not found")
-    
+
     if not argparse_args.api_credentials_file:
         raise FileNotFoundError(
             f"Error: API credentials file '{argparse_args.api_credentials_file}' not found! Go to https://developer.spotify.com/documentation/web-api to create one")
-    
+
     api_obj = json.load(open(argparse_args.api_credentials_file))
 
     spotify_tracks_and_track_ids = get_track_ids_from_csv(
         argparse_args.csv_file, api_obj)
 
+    playlist_description = f"Playlist created on {
+        datetime.datetime.now().isoformat()} \n Created by {socket.gethostname()}"
+
     new_playlist_id = create_playlist_from_tracks(
-        spotify_tracks_and_track_ids, "Test Playlist", socket.gethostname(), api_obj)
-    
+        spotify_tracks_and_track_ids, "Test Playlist", playlist_description, api_obj)
+
     print(f"Created new playlist with ID {new_playlist_id}")
